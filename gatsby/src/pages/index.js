3293f90a88798fbbaa3bf12144d53b37677ef81f
stordahl <jacob@blkcatstudio.com>
2020-10-22 19:48:22 -0500

    build :bug: fix

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import styled from "styled-components"
 
-import ItemThumbnail from '../components/ItemThumbnail';
+import ItemThumbnail from '../components/itemThumbnail';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
