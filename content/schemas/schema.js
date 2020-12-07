c83b1c22b1434bf36baef121cf9a7a01b663a54e
stordahl <jacob@blkcatstudio.com>
2020-12-07 17:19:54 -0600

    Updates to Sanity Studio

--- content/schemas/schema.js
+++ content/schemas/schema.js
@@ -7,7 +7,6 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import blockContent from './blockContent'
 import category from './category'
 import product from './product'
-import vendor from './vendor'
 import productVariant from './productVariant'
 
 import localeString from './locale/String'
@@ -24,7 +23,6 @@ export default createSchema({
     // The following are document types which will appear
     // in the studio.
     product,
-    vendor,
     category,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
